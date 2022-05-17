c9b83bd1767a344651037cba7c6261eac4c57b03
Aycan <ladarklaster@gmail.com>
2022-05-17 16:53:17 +0200

    first comm

---
+++ fil-rouge-eva-sky-V2/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Eva Sky</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">
+                    <img src="Assets/img/Eva3.png" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="boutique.php">
+                    Boutique
+                </a>
+            </li>
+            <li>
+                <a href="vote.php">
+                    Vote
+                </a>
+            </li>
+            <li>
+                <a href="forum.php">
+                    Forum
+                </a>
+            </li>
+            <li>
+                <a href="reglement.php">
+                    Reglement
+                </a>
+            </li>
+            <li>
+                <a href="connexion.php" id="bleu">
+                    Ce connecter
+                </a>
+            </li>
+        </ul>
+        <hr>
+    </nav>
+
+    <section>
+        <div>
+            <h1>
+                Bienvenue !
+            </h1>
+            <p>
+                Rejoins les 2600 joueur inscrit
+            </p>
+            <a href="">
+                Discord
+            </a>
+        </div>
+        <div>
+            <img src="Assets/img/Eva3.png" alt="">
+            <h2>
+                Play.eva-sky.fr
+            </h2>
+            <p>
+                Joueur en ligne : 17
+            </p>
+        </div>
+    </section>
+</body>
+</html>
